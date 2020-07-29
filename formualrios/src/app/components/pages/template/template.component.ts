f5577dfb97695030b6cc883fa3ffab360feb62e1
Yoe Cardenas <yoe.cardenas@cun.edu.co>
2020-07-29 15:47:53 -0500

    Se agrega servicio pais para extraer listado de paises

--- formualrios/src/app/components/pages/template/template.component.ts
+++ formualrios/src/app/components/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, CheckboxControlValueAccessor } from '@angular/forms';
+import { NgForm} from '@angular/forms';
+import { PaisService } from '../../../services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -14,9 +15,12 @@ export class TemplateComponent implements OnInit {
     correo: 'yac8807@gmail.com'
   };
 
-  constructor() { }
+  constructor( private paisService: PaisService ) { }
 
   ngOnInit(): void {
+    this.paisService.getPaises().subscribe(paises => {
+      console.log(paises);
+    });
   }
 
   guardar(form: NgForm){
