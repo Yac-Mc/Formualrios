889a06891acd230ed2e49d176cfbbf1c80b86e20
Yoe Cardenas <yoe.cardenas@cun.edu.co>
2020-07-29 18:45:22 -0500

    Se agrega ReactiveFormsModule al app.module.ts y se agregan validaciones en el componente del formulario reactivo

--- formualrios/src/app/components/pages/reactive/reactive.component.ts
+++ formualrios/src/app/components/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor( private formBuilder: FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
+  }
+
+  getNoValido(input: string) {
+    return this.form.get(input).invalid && this.form.get(input).touched;
+  }
+
+  crearFormulario(){
+    this.form = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      correo: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]]
+    });
+  }
+
+  guardar(){
+    console.log(this.form);
+    if ( this.form.invalid ){
+      return Object.values( this.form.controls ).forEach(control => {
+        control.markAllAsTouched();
+      });
+    }
   }
 
 }
