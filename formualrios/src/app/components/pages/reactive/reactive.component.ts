c43d045ef0356974efa5e176977466a93fa9545b
Yoe Cardenas <yoe.cardenas@cun.edu.co>
2020-07-30 19:56:01 -0500

    Se agregan validaciones sincronas y asincornas al formualrio. se agregan agrupaciones de campos entre otros

--- formualrios/src/app/components/pages/reactive/reactive.component.ts
+++ formualrios/src/app/components/pages/reactive/reactive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from '../../../services/validadores.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-reactive',
@@ -10,23 +12,79 @@ export class ReactiveComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) {
+  constructor( private formBuilder: FormBuilder, private validadores: ValidadoresService) {
     this.crearFormulario();
+    this.cargarDataFormulario();
+    this.crearListeners();
   }
 
   ngOnInit(): void {
   }
 
   getNoValido(input: string) {
+
+    if (input === 'password2'){
+      const pass1 = this.form.get('password1').value;
+      const pass2 = this.form.get('password2').value;
+
+      return (pass1 === pass2 ) ? false : true;
+    }
     return this.form.get(input).invalid && this.form.get(input).touched;
+  }
+
+  get getPasaTiempos(){
+    return this.form.get('pasaTiempos') as FormArray;
   }
 
   crearFormulario(){
     this.form = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
-      apellido: ['', [Validators.required, Validators.minLength(5)]],
-      correo: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]]
+      apellido: ['', [Validators.required, Validators.minLength(5), this.validadores.noCardenas]],
+      correo: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      usuario: ['', [Validators.required] , this.validadores.existeUsuario ],
+      password1: ['', [Validators.required]],
+      password2: ['', [Validators.required]],
+      direccion: this.formBuilder.group({
+        departamento: ['', [ Validators.required ]],
+        ciudad: ['', [ Validators.required ]]
+      }),
+      pasaTiempos: this.formBuilder.array([])
+    }, {
+      validators: [this.validadores.passwordsIguales('password1', 'password2')]
+    }
+    );
+  }
+
+  cargarDataFormulario(){
+    // this.form.setValue({
+    this.form.reset({
+      nombre: 'Yoe',
+      apellido: null,
+      correo: 'yac8807@gmail.com',
+      password1: '123',
+      password2: '123',
+      direccion: {
+        departamento: 'Bogotá',
+        ciudad: 'Bogotá'
+      }
     });
+  }
+
+  crearListeners(){
+
+    // this.form.valueChanges.subscribe( valor => console.log(valor));
+
+    // this.form.statusChanges.subscribe( status => console.log({status}) );
+
+    this.form.get('apellido').valueChanges.subscribe(console.log);
+  }
+
+  agregarPasaTiempo(){
+    this.getPasaTiempos.push(this.formBuilder.control(''));
+  }
+
+  borrarPasaTiempo(i: number){
+    this.getPasaTiempos.removeAt(i);
   }
 
   guardar(){
@@ -36,6 +94,8 @@ export class ReactiveComponent implements OnInit {
         control.markAllAsTouched();
       });
     }
+
+    // this.form.reset();
   }
 
 }
---
+++ formualrios/src/app/services/validadores.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'dns';
+import { rejects } from 'assert';
+
+interface ErrorValidate {
+  [s: string]: boolean
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidadoresService {
+
+  constructor() { }
+
+  noCardenas( control: FormControl): ErrorValidate {
+    if (control.value?.toLowerCase() === 'cardenas'){
+      return {
+        noCardenas: true
+      };
+    }
+
+    return null;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+      if (pass1Control.value === pass2Control.value){
+        pass2Control.setErrors(null);
+      } else{
+        pass2Control.setErrors({noEsIgual: true});
+      }
+    };
+  }
+
+  existeUsuario(control: FormControl): Promise<ErrorValidate> | Observable<ErrorValidate> {
+
+    if (!control.value){
+      return Promise.resolve(null);
+    }
+
+    return new Promise( (resolve, reject) => {
+
+      setTimeout(() => {
+        if (control.value?.toLowerCase() === 'yac'){
+          resolve({existe: true});
+        } else{
+          resolve(null);
+        }
+      }, 3500);
+
+    });
+  }
+}
